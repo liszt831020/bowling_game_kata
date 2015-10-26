6d1cc6345a2b406792b5f38f95053014d2ce4e5d
unknown <user@801-1-05.ntnu.edu.tw>
2015-10-26 12:00:06 +0800

    FINAL_last

--- bowling/bowling/Game.cs
+++ bowling/bowling/Game.cs
@@ -15,37 +15,40 @@ namespace bowling
         bool DoubleStrike = false;
         public void SetPins(int p)
         {
-            FinalScore_+=p;
-            if (SpareCheck == true)
-            {
                 FinalScore_ += p;
-                SpareCheck = false;
-            }
-            if (StrikeCheck != -1)
-            {
-                FinalScore_ += p;
-                StrikeCheck++;
-                if (DoubleStrike == true)
+                //if(FrameCount<10)
+                if (SpareCheck == true)
                 {
                     FinalScore_ += p;
-                    StrikeCheck = 1;
-                    DoubleStrike = false;
+                    SpareCheck = false;
                 }
-                if (p == 10) DoubleStrike = true;
-                if (StrikeCheck == 2)  StrikeCheck = -1;
+                if (StrikeCheck != -1)
+                {
+                    FinalScore_ += p;
+                    StrikeCheck++;
+                    if (DoubleStrike == true)
+                    {
+                        FinalScore_ += p;
+                        StrikeCheck = 1;
+                        DoubleStrike = false;
+                    }
+                    if (p == 10) DoubleStrike = true;
+                    if (StrikeCheck == 2) StrikeCheck = -1;
+                }
+                if (LastPin == -1 && p == 10)
+                {
+                    FrameCount++;
+                    StrikeCheck = 0;
+                }
+                if (LastPin == -1 && p != 10) LastPin = p;
+                else
+                {
+                    if (LastPin + p == 10) SpareCheck = true;
+                    LastPin = -1;
+                    FrameCount++;
+                }
             }
-            if (LastPin == -1 && p == 10)
-            {
-                FrameCount++;
-                StrikeCheck = 0; 
-            }
-            if (LastPin == -1 && p!=10) LastPin = p;
-            else
-            {
-                if (LastPin + p == 10)   SpareCheck = true;
-                LastPin = -1;
-                FrameCount++;
-            }
+            
         }
 
         public int GetFinalScore()
